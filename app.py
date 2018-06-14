d069e46406628c4c7b53a08cb835fb53615c1339
james-chege <jymeschege@gmail.com>
2018-06-14 09:28:45 +0300

    Setup water pan capacity and location + modify water pan details

--- app.py
+++ app.py
@@ -42,17 +42,30 @@ def proposals():
 @app.route("/waterpans")
 @login_required
 def waterpans():
-	return render_template("waterpans.html")
+	# load local json for water pans
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, "static/files", "waterpan.json")
+    data = json.load(open(json_url))
+    json_url_location = os.path.join(SITE_ROOT, "static/files", "location.json")
+    location = json.load(open(json_url_location))
+    return render_template("waterpans.html", data=data, location=location)
 
 
 @app.route("/apan/<string:name>", methods=['GET'])
 @login_required
 def apan(name):
-	# load local json for water pans
+	# load local jsons
 	SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-	json_url = os.path.join(SITE_ROOT, "static/files", "waterpan.json")
+	json_url = os.path.join(SITE_ROOT, "static/files", "capacity.json")
 	data = json.load(open(json_url))
-	return render_template("apan.html", name=name)
+	json_url_location = os.path.join(SITE_ROOT, "static/files", "location.json")
+	location = json.load(open(json_url_location))
+	json_url_pan = os.path.join(SITE_ROOT, "static/files", "waterpan.json")
+	waterpan = json.load(open(json_url_pan))
+	json_url_user = os.path.join(SITE_ROOT, "static/files", "users.json")
+	user = json.load(open(json_url_user))
+
+	return render_template("apan.html", name=name, data=data, location=location, waterpan=waterpan, user=user)
 
 @app.route("/addpan", methods=["POST", "GET"])
 @login_required
